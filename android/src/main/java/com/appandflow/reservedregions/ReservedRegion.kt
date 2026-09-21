5dbcc0699ce556e360f9a98b83470bbb34e21e6e
Janic Duplessis <janicduplessis@gmail.com>
2026-09-20 21:30:00 -0400

    refactor: use App and Flow Android namespace

--- android/src/main/java/com/appandflow/reservedregions/ReservedRegion.kt
+++ android/src/main/java/com/appandflow/reservedregions/ReservedRegion.kt
@@ -1,4 +1,4 @@
-package com.reservedregions
+package com.appandflow.reservedregions
 
 import android.graphics.Rect
 
--- android/src/main/java/com/appandflow/reservedregions/ReservedRegionsView.kt
+++ android/src/main/java/com/appandflow/reservedregions/ReservedRegionsView.kt
@@ -1,4 +1,4 @@
-package com.reservedregions
+package com.appandflow.reservedregions
 
 import android.content.Context
 import android.graphics.Rect
